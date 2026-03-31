8fe81b68c6f2d6e89ed456c7a7c70fed05a59a26
Naufal Rafi Haryanto <Celtdinho@gmail.com>
2026-03-30 16:14:03 -0800

    Praktikum 3 - View Layout dan View Cell

---
+++ ci4/app/Models/ArtikelModel.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class ArtikelModel extends Model
+{
+    protected $table = 'artikel';
+    protected $primaryKey = 'id';
+
+    protected $allowedFields = [
+        'judul',
+        'isi',
+        'slug',
+        'created_at'
+    ];
+
+    protected $useTimestamps = true;
+}
